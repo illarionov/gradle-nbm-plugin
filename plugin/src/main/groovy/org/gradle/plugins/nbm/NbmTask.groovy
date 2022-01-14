79ad8911e86610bf34fa6dca5454c0bd6edad3fb
Alexey Illarionov <alexey@0xdc.ru>
2022-01-14 15:13:09 +0300

    Remove NbmTask.upToDateWhen { false }

--- plugin/src/main/groovy/org/gradle/plugins/nbm/NbmTask.groovy
+++ plugin/src/main/groovy/org/gradle/plugins/nbm/NbmTask.groovy
@@ -10,9 +10,6 @@ import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 class NbmTask extends ConventionTask {
-    public NbmTask() {
-        outputs.upToDateWhen { false }
-    }
 
     @OutputFile
     File getOutputFile() {
